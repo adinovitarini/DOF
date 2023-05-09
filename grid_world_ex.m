2f8030374cd26ee5722b2a2514575b6d253ae71d
Adi Novitarini <adinovitarini@gmail.com>
2023-05-09 16:21:59 +0700

    Update grid_world_ex.m

--- grid_world_ex.m
+++ grid_world_ex.m
@@ -1,1 +1,22 @@
-
+clear all;clc
+env = rlPredefinedEnv("BasicGridWorld");
+env.ResetFcn = @() 2;
+rng(0)
+qTable = rlTable(getObservationInfo(env),getActionInfo(env));
+qRepresentation = rlQValueRepresentation(qTable,getObservationInfo(env),getActionInfo(env));
+qRepresentation.Options.LearnRate = 1;
+agentOpts = rlQAgentOptions;
+agentOpts.EpsilonGreedyExploration.Epsilon = .04;
+qAgent = rlQAgent(qRepresentation,agentOpts);
+trainOpts = rlTrainingOptions;
+trainOpts.MaxStepsPerEpisode = 10;
+trainOpts.MaxEpisodes= 200;
+trainOpts.StopTrainingCriteria = "AverageReward";
+trainOpts.StopTrainingValue = 11;
+trainOpts.ScoreAveragingWindowLength = 30;
+doTraining = false;
+trainingStats = train(qAgent,env,trainOpts);
+plot(env)
+env.Model.Viewer.ShowTrace = true;
+env.Model.Viewer.clearTrace;
+sim(qAgent,env)
